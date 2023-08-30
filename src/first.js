819945de18c4ed34b09afca074e0812380a86117
Chris Hammock <chris@crshmk.com>
2023-08-30 20:16:31 +0700

    add instructions to first

--- src/first.js
+++ src/first.js
@@ -1,3 +1,20 @@
+/**
+ * return the first item in a collection, or an empty object  
+ *
+ * @param {Array} 
+ * @return {any} the first item or an empty object
+ * 
+ * @example 
+ *   const users = [{ name: 'Jo' }]
+ *   first(users)
+ *   // -> { name: 'Jo' }] 
+ *
+ *   first([])
+ *   // -> {} 
+ *
+ *   first(undefined)
+ *   // -> {} 
+ */
 import { always, both, head, ifElse, is } from 'ramda'
 import { isPresent } from './isPresent'
 
